c7418721a73fe345326a7a339c68ed9b594a793e
Kathleen Cachel <Kathleen.cachel@gmail.com>
2019-04-15 22:23:16 -0400

    Got a bar graph working with different colors for each field and gender. Now need to combine fields and or manually color gender.

---
+++ 2019-04-16/tidy_tuesday_4_16_2019.R
@@ -0,0 +1,45 @@
+# Kathleen Cachel
+
+library(tidyverse)
+library(wesanderson)
+women_research_raw <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-16/Economist_women-research.csv")
+
+research_titles <- c("country",
+  "Health sciences",
+  "Physical sciences",
+  "Engineering",
+  "Computer science, maths",
+  "Women inventors")
+
+# remove rows with NA values
+# update Column Names
+women_research_clean <- women_research_raw %>% 
+  na.omit() %>% 
+  set_names(nm = research_titles) %>% 
+  filter(country != "Country") %>% 
+  gather(field, percent, `Health sciences`:`Women inventors`)
+
+#make men version
+men_research_clean <- women_research_clean %>%
+  mutate(percent = 1 - as.numeric(percent))
+
+#create new gender column
+women_research_clean$gender <- "female"
+men_research_clean$gender <- "male"
+
+#update type in women data frame
+women_research_clean$percent <- as.numeric(women_research_clean$percent)
+
+#union rows to make one big tidy data set
+research_clean <- union(women_research_clean, men_research_clean)
+
+#plotting
+d <- with(research_clean, research_clean[order(country, gender, field),])
+#d <- subset.data.frame(d, country == "Australia")
+united_d <-  unite(d, field_gender, field, gender, sep = ".", remove = FALSE)
+
+ggplot(data=united_d, aes(x=field, y=percent, fill=field_gender)) + 
+  geom_bar(stat="identity") + 
+  facet_grid(~country)
+
+
